061215f1214d289bcd4ebb1316a8bf45fa669337
sourabhsodhi <sodhi.sourabh@gmail.com>
2021-05-22 23:19:05 +0530

    First Commit

---
+++ src/main/java/NextMaven_test/Maaavn_test/App.java
@@ -0,0 +1,13 @@
+package NextMaven_test.Maaavn_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/NextMaven_test/Maaavn_test/First_class.java
@@ -0,0 +1,30 @@
+package NextMaven_test.Maaavn_test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;  
+  
+public class First_class {  
+  
+    public static void main(String[] args) throws InterruptedException {  
+        
+    // declaration and instantiation of objects/variables  
+    //System.setProperty("webdriver.chrome.driver", "D:\\Jars and softwares\\chromedriver.exe");  
+
+    	WebDriverManager.chromedriver().setup();
+    ChromeDriver driver=new ChromeDriver();  
+      
+// Launch website  
+    driver.navigate().to("http://www.google.com/");  
+
+    
+          
+    // Click on the search text box and send value  
+    driver.findElement(By.name("q")).sendKeys("javatpoint tutorials"+ "\n");  
+          
+    }  
+  
+}  
